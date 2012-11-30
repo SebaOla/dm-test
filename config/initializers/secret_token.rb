d114ee77d94619986c96ea06214889c9bedb6c5a
Sebastian Olaizola <sebaolaizola@gmail.com>
2012-11-30 11:38:01 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DineromailTest::Application.config.secret_token = 'cbf185ded091867dbcb31b84a349e6ee042d048db032a5c015ca46d6d7cbab1cef3e0ff539e57fb1eeec6fd5d77bba483fcceaaf3796453e103b7d194bd421dc'
